4a0db4cd9756ca52ae8f72715325186a36226cc7
sprisca <priscasaina5@gmail.com>
2026-03-05 12:05:56 +0100

    modification

--- backend/server/scripts/populateDatabase.js
+++ backend/server/scripts/populateDatabase.js
@@ -6,13 +6,15 @@ const users = [
     firstName: 'Tony',
     lastName: 'Stark',
     email: 'tony@stark.com',
-    password: 'password123'
+    password: 'password123',
+    userName: 'Iron'
   },
   {
     firstName: 'Steve',
     lastName: 'Rogers',
     email: 'steve@rogers.com',
-    password: 'password456'
+    password: 'password456',
+    userName: 'Captain'
   }
 ]
 
